cc2df4f5889b5420f3d6c8078a6c23722eb9b6da
Jhunie <60381474+jhunielh07@users.noreply.github.com>
2020-11-18 10:44:25 +0800

    TemplateState

--- contracts/src/main/kotlin/com/template/states/TemplateState.kt
+++ contracts/src/main/kotlin/com/template/states/TemplateState.kt
@@ -5,13 +5,27 @@ import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
+import java.net.Inet4Address
 
 // *********
 // * State *
 // *********
+@CordaSerializable
+
+enum class statusType {
+    Single,
+    Married
+}
+
 @BelongsToContract(TemplateContract::class)
-data class TemplateState(val nameA:Party) : ContractState{
+data class TemplateState(val nameA:Party,
+                         val nameB:Party,
+                         val age: Int,
+                         val gender: Int,
+                         val address: String,
+                         val status: statusType) : ContractState{
 
     override val participants: List<AbstractParty>
-        get() = listOf(nameA)
+        get() = listOf(nameA,nameB)
 }
