33fe14aa8ed5983808cf9f34b8a48d5c8d4ec0af
paulrndp <paulrndp@gmail.com>
2020-12-02 11:05:12 +0800

    DeleteUserFlow global function

--- workflows/src/main/kotlin/com/template/flows/DeleteUserFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/DeleteUserFlow.kt
@@ -3,6 +3,7 @@ package com.template.flows
 import co.paralleluniverse.fibers.Suspendable
 import com.template.contracts.UserContract
 import com.template.states.UserState
+import functions.FunctionFlow
 import net.corda.core.contracts.Command
 import net.corda.core.contracts.StateAndRef
 import net.corda.core.contracts.UniqueIdentifier
@@ -13,74 +14,55 @@ import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
-
+import net.corda.core.utilities.loggerFor
 
 @InitiatingFlow
 @StartableByRPC
-class DeleteUserFlow (private val linearId: UniqueIdentifier) : FlowLogic<SignedTransaction>() {
+class DeleteUserFlow (private val linearId: UniqueIdentifier) : FunctionFlow() {
 
-    private fun userStates(settle: StateAndRef<UserState>): UserState {
-        return UserState(
-                name = settle.state.data.name,
-                age = settle.state.data.age,
-                address = settle.state.data.address,
-                status = settle.state.data.status,
-                gender = settle.state.data.gender,
-                node = ourIdentity,
-                linearId = linearId,
-                delete = true,
-                participants = settle.state.data.participants
-        )
+    private companion object{
+        val log = loggerFor<CreateUserOwnNodesWithoutOtherParty>()
     }
+
     @Suspendable
     override fun call(): SignedTransaction {
+
+        fun newUserState(settle: StateAndRef<UserState>): UserState {
+            return UserState(
+                    name = settle.state.data.name,
+                    age = settle.state.data.age,
+                    address = settle.state.data.address,
+                    status = settle.state.data.status,
+                    gender = settle.state.data.gender,
+                    node = ourIdentity,
+                    linearId = linearId,
+                    delete = true,
+                    participants = settle.state.data.participants
+            )
+        }
         val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
         val settle = serviceHub.vaultService.queryBy<UserState>(queryCriteria).states.single()
+        val sessions: List<FlowSession> = (newUserState(settle).participants - ourIdentity).map { initiateFlow(it) }.toSet().toList()
+        val command = Command(UserContract.Commands.Update(), newUserState(settle).participants.map { it.owningKey })
 
-        val transaction: TransactionBuilder = transaction()
+        log.info("Building Transaction")
+        val utx = TransactionBuilder(myNotary)
+                .addInputState(settle)
+                .addOutputState(newUserState(settle), UserContract.ID)
+                .addCommand(command)
 
-        val signedTransaction: SignedTransaction = verifyAndSign(transaction)
-
-        val sessions: List<FlowSession> = (userStates(settle).participants - ourIdentity).map { initiateFlow(it) }.toSet().toList()
-
-        val transactionSignedByAllParties: SignedTransaction = collectSignature(signedTransaction, sessions)
-
-        return recordTransaction(transactionSignedByAllParties, sessions)
+        return signCollectNotarize(
+                utx = utx,
+                session = sessions,
+                log = log
+        )
 
     }
-    private fun transaction(): TransactionBuilder {
-        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
-        val settle = serviceHub.vaultService.queryBy<UserState>(queryCriteria).states.single()
-
-        val notary: Party = serviceHub.networkMapCache.notaryIdentities.first()
-        val updateCommand = Command(UserContract.Commands.Update(), userStates(settle).participants.map { it.owningKey })
-        val builder = TransactionBuilder(notary = notary)
-        builder.addInputState(settle)
-        builder.addOutputState(userStates(settle), UserContract.ID)
-        builder.addCommand(updateCommand)
-        return builder
-    }
-
-    private fun verifyAndSign(transaction: TransactionBuilder): SignedTransaction {
-        transaction.verify(serviceHub)
-        return serviceHub.signInitialTransaction(transaction)
-    }
-
-    @Suspendable
-    private fun collectSignature(
-            transaction: SignedTransaction,
-            sessions: List<FlowSession>
-    ): SignedTransaction = subFlow(CollectSignaturesFlow(transaction, sessions))
-
-    @Suspendable
-    private fun recordTransaction(transaction: SignedTransaction, sessions: List<FlowSession>): SignedTransaction =
-            subFlow(FinalityFlow(transaction, sessions))
 }
 @InitiatedBy(DeleteUserFlow::class)
 class DeleteUserFlowResponder(val flowSession: FlowSession) : FlowLogic<SignedTransaction>() {
 
-    @Suspendable
-    override fun call(): SignedTransaction {
+    @Suspendable  override   fun call(): SignedTransaction {
         val signTransactionFlow = object : SignTransactionFlow(flowSession) {
             override fun checkTransaction(stx: SignedTransaction) = requireThat {
             }
